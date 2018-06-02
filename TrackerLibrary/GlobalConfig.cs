5968945598bac08bf0f030b3429d71c5f9d2c6d5
Richard Nguyen <reechardc@gmail.com>
2018-06-02 15:12:59 -0700

    Add project files.

---
+++ TrackerLibrary/GlobalConfig.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public static class GlobalConfig
+    {
+        public static List<IDataConnection> Connections { get; private set; }
+
+        public static void InitalizeConnections(bool database, bool textFiles)
+        {
+
+        }
+    }
+}
